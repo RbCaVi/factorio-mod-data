65b94a6efdc67acf0a4b86a5202d672d7bc30399
RbCaVi <92527341+RbCaVi@users.noreply.github.com>
2024-05-15 12:54:26 -0400

    Revert copymods.sh

--- copymods.sh
+++ copymods.sh
@@ -3,7 +3,7 @@ rootpath=$(realpath .)
 
 
 for file in `ls packs`; do
-  cp -r "packs/$file" "$rootpath/SEJS/"
+  cp -r "packs/$file" "$rootpath/SEJS/$file"
 done
 
 cp modsjson.txt "$rootpath/SEJS/mods.json"

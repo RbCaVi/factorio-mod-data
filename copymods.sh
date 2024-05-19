e20c57e8b7ed5207f7d405c4a7ddafa0b76b5a34
RbCaVi <92527341+RbCaVi@users.noreply.github.com>
2024-05-18 21:23:29 -0400

    mv doesn't take -r

--- copymods.sh
+++ copymods.sh
@@ -4,7 +4,7 @@ rootpath=$(realpath .)
 
 for file in `ls packs`; do
   name=$(cat "packs/$file/pack.json"|jq -r '.name')
-  mv -r "packs/$file/$name" "$rootpath/SEJS/$file"
+  mv "packs/$file/$name" "$rootpath/SEJS/$file"
 done
 
 cp modsjson.txt "$rootpath/SEJS/mods.json"

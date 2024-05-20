1fdc8e2b1e421874b5e6b48d35de477216812abd
RbCaViTerminal <robert@robertvail.info>
2024-05-20 09:03:17 -0400

    no SEJS

--- copymods.sh
+++ copymods.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 rootpath=$(realpath .)
 
+mkdir -p "assets"
 
 for file in `ls packs`; do
   name=$(cat "packs/$file/pack.json"|jq -r '.name')
-  mv "packs/$file/$name" "$rootpath/SEJS/$file"
+  mv "packs/$file/$name" "$rootpath/assets/$file"
 done
 
-cp modsjson.txt "$rootpath/SEJS/mods.json"
+cp modsjson.txt "$rootpath/assets/mods.json"
